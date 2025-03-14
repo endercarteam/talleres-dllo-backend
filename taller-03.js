c94ca4b9658bd359c3ede74e52f2d7537f4ef350
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2025-03-14 13:10:08 -0500

    Update taller-03.js

--- taller-03.js
+++ taller-03.js
@@ -1,7 +1,9 @@
+let list1 = [2,7,11,15];
+let list2 = [3,4,2];
 function desglosarString(str, req){
-    let cont = 0
+    let cont = 0;
     let vocales = "aeiouáéíóúü";
-    let consonantes ="bcdfghjklmnñpqrstvwxyz"
+    let consonantes ="bcdfghjklmnñpqrstvwxyz";
     str = str.toLowerCase();  
     
     if(req == "vocales"){
@@ -20,6 +22,58 @@ function desglosarString(str, req){
 
 return cont 
 }
+function twoSum(num, objt){
+   
 
-console.log(desglosarString("murcielagos","vocales"))
-console.log(desglosarString("murcielagos","consonantes"))
+    for(let i = 0; i < num.length; i++){
+        for(let j = i + 1; j < num.length; j++){
+            
+            if(num[i] + num[j] === objt){
+                return [i, j]
+            }
+        }
+    }
+
+    
+}
+function conversionRomana(rom) {
+    
+    rom = rom.toUpperCase();
+
+    
+    const valores = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    
+    
+
+    let resultado = 0;
+
+    
+    for (let i = 0; i < rom.length; i++) {
+        
+        if ( valores[rom[i]] < valores[rom[i + 1]]) {
+            resultado -= valores[rom[i]];
+        } else {
+            
+            resultado += valores[rom[i]];
+        }
+    }
+
+    return resultado;
+}
+console.log(desglosarString("murcielagos","vocales"));
+console.log(desglosarString("murcielagos","consonantes"));
+console.log(twoSum(list1, 9));
+console.log(twoSum(list2, 6));
+console.log(conversionRomana("III"));
+console.log(conversionRomana("XIV"));
+console.log(conversionRomana("MMXXIV"));
+console.log(conversionRomana("MXMVII"));
